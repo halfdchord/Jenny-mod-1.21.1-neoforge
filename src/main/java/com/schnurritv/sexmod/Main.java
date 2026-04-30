7e9c9370e1a9898e0b10cf98ed4af40f3930e6c5
HDC <146910015+halfdchord@users.noreply.github.com>
2026-04-29 20:52:41 -0400

    Fuck with main.java
    Thank you copilot

--- src/main/java/com/schnurritv/sexmod/Main.java
+++ src/main/java/com/schnurritv/sexmod/Main.java
@@ -2,10 +2,11 @@ package com.schnurritv.sexmod;
 
 import com.schnurritv.sexmod.networking.NetworkHandler;
 import com.schnurritv.sexmod.entity.EntityRegistry;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,11 +15,11 @@ public class Main {
     public static final String MODID = "sexmod";
     public static final Logger LOGGER = LogManager.getLogger();
 
-    public Main() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        EntityRegistry.register(FMLJavaModLoadingContext.get().getModEventBus());
-        net.minecraftforge.fml.ModLoadingContext.get().registerConfig(net.minecraftforge.fml.config.ModConfig.Type.CLIENT, SexModConfig.SPEC);
-        MinecraftForge.EVENT_BUS.register(this);
+    public Main(ModContainer modContainer) {
+        IEventBus modEventBus = modContainer.getEventBus();
+        modEventBus.addListener(this::setup);
+        EntityRegistry.register(modEventBus);
+        NeoForge.EVENT_BUS.register(this);
     }
 
     private void setup(final FMLCommonSetupEvent event) {
